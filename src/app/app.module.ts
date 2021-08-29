955a59415cfd3dda810632b8696d9792e4bc489a
Juan Gutierrez <gutierrezbj@gmail.com>
2021-08-29 11:07:00 +0200

    mi primer commit con Fer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorModule } from './contador/contador/contador.module';
 import { HeroesModule } from './heroes/heroe/heroes.module';
 
 //Tarea: 
@@ -15,7 +16,8 @@ import { HeroesModule } from './heroes/heroe/heroes.module';
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
